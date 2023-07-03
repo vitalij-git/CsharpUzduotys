f804e9fedb4080af96ca949d02d4018a76f74d48
vitalij-git <vitalijusdubovikov@gmail.com>
2023-07-03 21:32:01 +0300

    dictonary

--- 16. Dictionary/16. Dictionary/Program.cs
+++ 16. Dictionary/16. Dictionary/Program.cs
@@ -4,7 +4,48 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //RetrieveTownPopuliation();
+            WordBook();
+            Console.ReadKey();
         }
+
+        static void WordBook()
+        {
+            Dictionary<string, int> users = new Dictionary<string, int>
+            {
+                {"Tomas",25},
+                {"Jonas",32},
+                {"Saulius",29}
+            };
+            foreach (var user in users)
+            {
+                Console.WriteLine($"{user.Key} {user.Value}");
+            }
+        }
+
+        static void RetrieveTownPopuliation()
+        {
+            Dictionary<string, int> townDictonary = new Dictionary<string, int>
+            {
+                {"vilnius", 500000 },
+                {"kaunas", 400000 },
+                {"siauliau", 300000}
+            };
+            Console.WriteLine("Iveskite miesta: ");
+            string town = Console.ReadLine().ToLower();
+
+            if(townDictonary.ContainsKey(town)) 
+            {
+                Console.WriteLine($"Gyventoju skaicius {townDictonary[town]}");
+            }
+            else
+            {
+                Console.WriteLine("Tokio miesto nera bazeje");
+            }
+           
+
+
+        }
+
     }
 }
