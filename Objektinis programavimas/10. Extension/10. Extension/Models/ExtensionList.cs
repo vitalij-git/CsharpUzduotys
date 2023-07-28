1e255e89e8bc73281bbf2f7a2b8698e1d105a6ac
vitalij-git <vitalijusdubovikov@gmail.com>
2023-07-28 22:06:45 +0300

    git

--- Objektinis programavimas/10. Extension/10. Extension/Models/ExtensionList.cs
+++ Objektinis programavimas/10. Extension/10. Extension/Models/ExtensionList.cs
@@ -6,11 +6,24 @@ using System.Threading.Tasks;
 
 namespace _10._Extension.Models
 {
-    public static class ExtensionList<T>
+    internal static class ExtensionList
     {
-        public static List<T> FindAndReturnIfEqualList() 
+        public static T FindAndReturnIfEqualList<T>(this List<T> checkList, T str )
         { 
-            return
+            return checkList.Contains(str)? str : default;
+        }
+
+        public static List<T> EveryOrherWordList<T>( this List<T> list)
+        {
+            var newList = new List<T>();
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (i == 0 || i % 2 == 0)
+                {
+                    newList.Add(list[i]);
+                }
+            }
+            return newList;
         }
 
     }
