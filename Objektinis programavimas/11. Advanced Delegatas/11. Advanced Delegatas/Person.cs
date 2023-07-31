3c84a8ba3c7e7e032de9aa018b7796722031d325
vitalij-git <vitalijusdubovikov@gmail.com>
2023-07-31 22:05:48 +0300

    delegate

---
+++ Objektinis programavimas/11. Advanced Delegatas/11. Advanced Delegatas/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11._Advanced_Delegatas
+{
+    internal class Person
+    {
+        public Person()
+        {
+        }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string  Name { get; set; }
+        public int Age { get; set; }
+    }
+}
--- Objektinis programavimas/11. Advanced Delegatas/11. Advanced Delegatas/Program.cs
+++ Objektinis programavimas/11. Advanced Delegatas/11. Advanced Delegatas/Program.cs
@@ -1,22 +1,46 @@
 ﻿using System.Reflection;
+using System.Text;
 
 namespace _11._Advanced_Delegatas
 {
     internal class Program
     {
-        private delegate string Person(string firstName, string lastName, int age);
+        private delegate string Person1(string firstName, string lastName, int age);
         private delegate int DelegateInteger(int number1, int number2);
-        private delegate List<int> DelegateList(List<int> numbers,int step);
+        public delegate List<int> DelegateList(List<int> numbers,int step);
         private delegate T GenericsDelegate<T>(T t);
+        private delegate bool Filter (Person person);
         static void Main(string[] args)
         {
-            var person = new Person(ShowPerson);
-            Console.WriteLine(person("Tomas","Jonaitis",25));
-            var delegateList = new DelegateList(PutNumbersInList);
-            delegateList += DelegateNewList;
-            Console.WriteLine(delegateList);
-           // var genericDelegate = new GenericsDelegate<T>(Generics<T>);
+            //var person = new Person1(ShowPerson);
+            //Console.WriteLine(person("Tomas","Jonaitis",25));
+            //var numbersSum = new DelegateInteger(SumReturn);
+            //Console.WriteLine(numbersSum(10,20));
+            //var delegateList = new DelegateList(PutNumbersInList);
+            //delegateList +=DelegateNewList;
+            //var genericDelegate = new GenericsDelegate<string>(Generics<string>);
+            //Console.WriteLine(genericDelegate);
 
+
+            Person1 human = delegate (string firstName, string lastName, int age)
+            {
+                return $"Frist name is {firstName}, last name is {lastName} and age is {age}";
+            };
+            Console.WriteLine(human("Jonas","Jonaitis",13));
+            List<Person> persons = new List<Person>();
+            var person = new Person("Simas", 25);
+            persons.Add(person);
+            var person1 = new Person("Kevin", 8);
+            persons.Add(person1);   
+            var person2 = new Person("Buzz", 66);
+            persons.Add(person2);
+            Filter isChild = delegate (Person person)
+            {
+                return IsChild(person);
+            };
+            DisplayPeople("Children", persons, isChild);
+            
+                
         }
 
         static string ShowPerson(string firstName, string lastName, int age) 
@@ -24,7 +48,7 @@ namespace _11._Advanced_Delegatas
             return $"Frist name is {firstName}, last name is {lastName} and age is {age}";
         }
 
-        static int NumbersReturn(int number1, int number2)
+        static int SumReturn(int number1, int number2)
         {
             return number1 + number2;
         }
@@ -39,17 +63,54 @@ namespace _11._Advanced_Delegatas
         static List<int> DelegateNewList(List<int> numbers, int step)
         {
             var newList = new List<int>();
+            StringBuilder str = new StringBuilder();
             for (int i = 0; i < numbers.Count(); i += step)
             {
-                newList.Add(numbers[i]);    
+                newList.Add(numbers[i]);
+                
             }
-            return newList;
+            return newList; 
         }
         
         static string Generics<T>(T generics)
         {
             return typeof(T).ToString();
         }
+
+        static bool IsChild (Person person)
+        {
+            if (person.Age < 18)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        static bool IsAdult(Person person)
+        {
+            if (person.Age>=18 && person.Age < 65)
+            {
+                return true;
+            }
+            return false;
+        }
         
+        static bool IsSenior(Person person)
+        {
+            if (person.Age >= 65)
+            {
+                return true;
+            }
+            return false;
+        }   
+
+        static void DisplayPeople(string title, List<Person> persons, Filter person)
+        {
+            foreach (var item in persons)
+            {
+                Console.WriteLine(person(item)); 
+            }
+            
+        }
     }
 }
